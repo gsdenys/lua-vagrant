0a80b149b7bfc1d4736a52577bf51e2c6b4dc8c6
Denys G. Santos <gsdenys@gmail.com>
2019-07-17 03:50:23 +0000

    finalizing luajit luarocks and openssl

--- Vagrantfile
+++ Vagrantfile
@@ -16,11 +16,13 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     vb.cpus = cpus
     vb.customize ["guestproperty", "set", :id, "/VirtualBox/GuestAdd/VBoxService/ — timesync-set-threshold", 10000]
   end
-
+  
   config.vm.box = "ubuntu/bionic64"
   config.vm.synced_folder source, "/lua"
 
+  for i in 8080..9999
+    config.vm.network :forwarded_port, guest: i, host: i
+  end
+
   config.vm.provision "shell", path: "provision.sh"
-    # env: { "HTTP_PROXY": ENV["HTTP_PROXY"], "HTTPS_PROXY": ENV["HTTPS_PROXY"]},
-    #:args => [version, cversion, utils, anreports, loglevel]
 end
--- provision.sh
+++ provision.sh
@@ -6,36 +6,47 @@ set -o errexit
 export DEBIAN_FRONTEND=noninteractive
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
-
-
-echo "*************************************************************************"
-echo "Installing Lua version: $LUA_VERSION"
-echo "*************************************************************************"
-
-
-sudo chown -R vagrant /usr/local
-
-
-sudo -E apt-get update
-sudo -E apt-get -y upgrade
-sudo -E apt-get install -qq gcc g++
-sudo -E apt-get install -qq httpie jq
-sudo -E apt-get install -qq git curl make pkg-config unzip apt-transport-https language-pack-en libssl-dev m4 cpanminus
-sudo -E apt-get install -qq lua5.1 luarocks luajit
-
-
-# Adjust PATH for future SSH
-echo "export PATH=/usr/local/bin:/usr/local/openresty/bin:/opt/stap/bin:/usr/local/stapxx:/usr/local/openresty/nginx/sbin:/usr/local/openresty/luajit/bin:\$PATH:" >> /home/vagrant/.bashrc
-
-# Do the same for root so we access to profiling tools
-echo "export PATH=/usr/local/bin:/usr/local/openresty/bin:/opt/stap/bin:/usr/local/stapxx:/usr/local/openresty/nginx/sbin:/usr/local/openresty/luajit/bin:\$PATH" >> /root/.bashrc
-# Set locale
-echo "export LC_ALL=en_US.UTF-8" >> /home/vagrant/.bashrc
-echo "export LC_CTYPE=en_US.UTF-8" >> /home/vagrant/.bashrc
-
 # Fix locale warning
 sudo echo "LC_CTYPE=\"en_US.UTF-8\"" >> /etc/default/locale
 sudo echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/default/locale
 
+# Set locale
+echo "export LC_ALL=en_US.UTF-8" >> /home/vagrant/.bashrc
+echo "export LC_CTYPE=en_US.UTF-8" >> /home/vagrant/.bashrc
+
 # Assign permissions to "vagrant" user
 sudo chown -R vagrant /usr/local
+
+sudo -E apt-get update
+sudo -E apt-get -y upgrade
+sudo -E apt-get install -qq gcc g++ 
+sudo -E apt-get install -qq httpie jq
+sudo -E apt-get install -qq git curl make cmake pkg-config unzip apt-transport-https language-pack-en libssl-dev m4 cpanminus
+
+#install openssl
+wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
+tar xvf openssl-1.1.1.tar.gz
+cd openssl-1.1.1
+sudo ./config -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
+sudo make
+sudo make install
+cd ..
+rm -rf openssl-1.1.1*
+
+#install luarocks and Luajit
+git clone https://github.com/torch/luajit-rocks.git
+cd luajit-rocks
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/opt/luarocks
+sudo make install
+cd ../../
+rm -rf luajit-rocks
+
+sudo chown -R vagrant /opt/luarocks
+ln -s /opt/luarocks/bin/luajit /opt/luarocks/bin/lua
+
+# Adjust PATH for future SSH
+echo "export PATH=/usr/local/bin:/opt/luarocks/bin:\$PATH:" >> /home/vagrant/.bashrc
+# Do the same for root so we access to profiling tools
+echo "export PATH=/usr/local/bin:/opt/luarocks/bin:\$PATH" >> /root/.bashrc
